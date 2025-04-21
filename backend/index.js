fef65d3f2abe90fe36c0702c981b1aebaa41b462
tejasbahurupi <tjbahurupi07@gmail.com>
2025-04-21 20:36:23 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,36 @@
-const express = require('express');
-const app=express();
-const  bodyParser=require('body-parser')
-const cors= require('cors'); 
-const AutherRouter=require('./Routes/AuthRouter');
-const PosterRouter=require('./Routes/DataRoute');
-const DataRouter=require('./Routes/SemesterRoute');
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const AutherRouter = require("./Routes/AuthRouter");
+const PosterRouter = require("./Routes/DataRoute");
+const DataRouter = require("./Routes/SemesterRoute");
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://marksheet-validation.pages.dev",
+];
 
-require('dotenv').config();
-require('./Models/db');
+require("dotenv").config();
+require("./Models/db");
 
-
-
-const PORT=process.env.PORT || 8080;
-
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
-app.use(cors());
-app.use('/auth', AutherRouter);
-app.use('/semester' , PosterRouter);
-app.use('/verify' ,DataRouter);
-app.listen(PORT , ()=>{
-    console.log(`Server is listening on ${PORT}`);
-})
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+app.use("/auth", AutherRouter);
+app.use("/semester", PosterRouter);
+app.use("/verify", DataRouter);
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
